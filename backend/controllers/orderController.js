6a9e5ff81781d926066d1ca8d4b5e9a544a58100
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-08-25 03:55:22 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,8 +38,8 @@ const placeOrder = async (req, res) => {
         })
         
           const session = await stripe.checkout.sessions.create({
-            success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `https://hungry-hub-frontend-wpi5.onrender.com/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `https://hungry-hub-frontend-wpi5.onrender.com/verify?success=false&orderId=${newOrder._id}`,
             line_items: line_items,
             mode: 'payment',
           });
