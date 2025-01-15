ab3317e932829d1b99b08774c9d1e1a254a7848b
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2025-01-15 15:25:08 +0530

    Deployement

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000";
+    const url = "https://hungry-hub-backend-r2gu.onrender.com";
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
