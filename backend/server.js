e77b824d39cde8f9fd4ac5a1cf825ce054ac7788
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2025-01-17 19:27:04 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000
 
 // middlewares
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://hungry-hub-eosin.vercel.app/" }));
 // app.use(cors({origin:"http://localhost:5174"}))
 // db connection
 connectDB()
